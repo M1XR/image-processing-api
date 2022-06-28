8bca7b20c08acfb83e58ec1e8e2c2c4111c7aa90
Mike Rossa <97miro@gmail.com>
2022-06-28 14:40:57 +0200

    delete redundant code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,7 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import image from './api/image';
-import path from 'path';
 
 const routes = express.Router();
-
-// send description File to Browser
-routes.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../', 'server.html'));
-});
 
 routes.use('/image', image);
 
